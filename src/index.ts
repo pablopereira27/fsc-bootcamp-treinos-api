cad675c281ba6d4c4fca6d75d4742d28414b04b0
Pablo Pereira <pablopereira27@gmail.com>
2026-03-01 13:43:28 -0300

    feat: add fastity api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async () => {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
